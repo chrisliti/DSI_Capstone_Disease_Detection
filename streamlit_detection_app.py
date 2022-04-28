800d651f8d682c2eefc155450a7961b4a69ca5f9
ChrisMbeva <chrismbev83@gmail.com>
2022-04-28 21:16:35 +0300

    Update streamlit_detection_app.py
    added detect button.

--- streamlit_detection_app.py
+++ streamlit_detection_app.py
@@ -9,36 +9,37 @@ import sys
 ## Upload video
 video_file = st.file_uploader('myvideo.mp4', type = ['mp4'])
 
-with open(video_file.name, "wb") as f:
-                    f.write(video_file.getbuffer())
+if st.button('Detect Disease'):
+  with open(video_file.name, "wb") as f:
+    f.write(video_file.getbuffer())
 
 
 
-## Run inference
-#os.chdir('/content/drive/MyDrive/Capstone-Project/Object-Detection/yolov5')
+  ## Run inference
+  #os.chdir('/content/drive/MyDrive/Capstone-Project/Object-Detection/yolov5')
 
-img_fdr= video_file.name
-weights_fdr="best.pt"
+  img_fdr= video_file.name
+  weights_fdr="best.pt"
 
-subprocess.run(f"python3 detect.py --weights {weights_fdr} --source {img_fdr}",shell=True)
-#print(f"python3 detect.py --weights {weights_fdr} --source {img_fdr}")
+  subprocess.run(f"python3 detect.py --weights {weights_fdr} --source {img_fdr}",shell=True)
+  #print(f"python3 detect.py --weights {weights_fdr} --source {img_fdr}")
 
-def get_subdirs(b='.'):
-    '''
-        Returns all sub-directories in a specific Path
-    '''
-    result = []
-    for d in os.listdir(b):
-        bd = os.path.join(b, d)
-        if os.path.isdir(bd):
-            result.append(bd)
-    return result
+  def get_subdirs(b='.'):
+      '''
+          Returns all sub-directories in a specific Path
+      '''
+      result = []
+      for d in os.listdir(b):
+          bd = os.path.join(b, d)
+          if os.path.isdir(bd):
+              result.append(bd)
+      return result
 
-def get_detection_folder():
-    '''
-        Returns the latest folder in a runs\detect
-    '''
-    return max(get_subdirs(os.path.join('runs', 'detect')), key=os.path.getmtime)
+  def get_detection_folder():
+      '''
+          Returns the latest folder in a runs\detect
+      '''
+      return max(get_subdirs(os.path.join('runs', 'detect')), key=os.path.getmtime)
 
-for vid in os.listdir(get_detection_folder()):
-  st.video(str(Path(f'{get_detection_folder()}') / vid))
+  for vid in os.listdir(get_detection_folder()):
+    st.video(str(Path(f'{get_detection_folder()}') / vid))
